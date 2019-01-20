d4555d887610d3a4817eecd93b3caf91036070ae
Starlet <lstar2397@naver.com>
2019-01-20 19:08:57 +0900

    Update main.c

--- src/main.c
+++ src/main.c
@@ -20,8 +20,17 @@ int main()
 
 char* ModifyString(char *prefix, char *string, int size)
 {
-	int ch, len;
-	bool isPrinted;
+	int ch, ch2, len;
+	bool isPrinted, is2Byte, *twoByte = (bool*)calloc(size, sizeof(bool));
+
+	for (size_t i = 0; i < strlen(string); i++)
+	{
+		if (((unsigned char)string[i] & 0x80) == 0x80)
+		{
+			twoByte[i + 1] = true;
+			i++;
+		}
+	}
 
 	isPrinted = false;
 	while (1)
@@ -40,22 +49,48 @@ char* ModifyString(char *prefix, char *string, int size)
 				len = strlen(string);
 				if (len > 0)
 				{
-					string[len - 1] = '\0';
-					printf("\b \b");
+					is2Byte = false;
+					if (1 < len && twoByte[len - 1])
+					{
+						twoByte[len - 1] = false;
+						string[len - 2] = '\0';
+						is2Byte = true;
+						printf("\b\b  \b\b");
+					}
+					if (!is2Byte)
+					{
+						string[len - 1] = '\0';
+						printf("\b \b");
+					}
 				}
 			}
 			else
 			{
 				len = strlen(string);
-				if (len < size - 1)
+				if (((unsigned char)ch & 0x80) == 0x80)
 				{
-					string[len] = ch;
-					string[len + 1] = '\0';
+					if (len < size - 2)
+					{
+						ch2 = _getch();
+						string[len] = ch;
+						string[len + 1] = ch2;
+						string[len + 2] = '\0';
+						twoByte[len + 1] = true;
+					}
+				}
+				else
+				{
+					if (len < size - 1)
+					{
+						string[len] = ch;
+						string[len + 1] = '\0';
+					}
 				}
 			}
 			isPrinted = false;
 		}
 	}
 	printf("\n");
+	free(twoByte);
 	return string;
 }
